c3fb29a3ddce7010583ac7b99f0611741b8e90cb
Hugo Tamayo <doctamayot@hotmail.com>
2019-03-13 21:41:21 -0500

    listo

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {counter: 0}
+  }
+
+  add(){
+    this.setState({ counter: this.state.counter + 1})
+  }
   render() {
-    return (
+     return (      
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.counter}</span>
+        <button onClick= {this.add.bind(this)} id="inc">Incrementa</button>
       </div>
     );
-  }
+    
+    
+}
 }
 
 export default App;
